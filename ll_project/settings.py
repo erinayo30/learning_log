6a50564641aae172c3ee1d7ef2e7590617cf7232
Joshua Farinmade <joshuafarinmade@gmail.com>
2026-04-03 19:53:09 +0100

    Deployment

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -128,3 +128,30 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Platform.sh setting
+from platformshconfig import Config
+
+config = Config()
+if config.is_valid_platform():
+    ALLOWED_HOSTS.append('.platformsh.site')
+
+    if config.appDir:
+        SATIC_ROOT = Path(config.appDir) /'static'
+    if config.projectEntropy:
+        SECRET_KEY = config.projectEntropy
+
+    if not config.in_build():
+        db_settings =config.credentials('database')
+        DATABASES ={
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': db_settings['path'],
+                'USER': db_settings['username'],
+                'PASSWORD': db_settings['password'],
+                'HOST': db_settings['host'],
+                'PORT': db_settings['port'],
+            },
+        }
+else:
+    pass
